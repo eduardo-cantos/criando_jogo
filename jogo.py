ad53df6d777b154c26f84c212c539e8cf196456f
Eduardo Cantos <eduardocantosmartins@gmail.com>
2025-11-17 09:56:42 -0300

    desenhando objetos

--- jogo.py
+++ jogo.py
@@ -17,4 +17,20 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+    pygame.draw.rect(tela,(255,0,0),(200,300,40,50)) # 1. tela → Surface onde o retângulo será desenhado (geralmente a tela principal do jogo).
+    # 2. (255,0,0) → Cor do retângulo em RGB (neste caso, vermelho).
+    # 3. (200,300,40,50) → Tupla que define o retângulo:
+    #    - 200 → posição X (horizontal, distância da borda esquerda).
+    #    - 300 → posição Y (vertical, distância do topo).
+    #    - 40  → largura do retângulo.
+    #    - 50  → altura do retângulo.
+
+    pygame.draw.circle(tela, (0,0,200),(300,260), 40)
+
+    pygame.draw.line(tela,(255,255,3), (390,0), (390,600), 5)
+
+
     pygame.display.update() #cada iteração do looping principal atualiza a tela do jogo
+
+    
